af2ead68388dad560b534e0615baaaed00b140a9
Ryan M. <rgmarlow88@gmail.com>
2024-10-18 01:43:12 -0700

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,99 @@
+from flask import (
+    Flask,
+    render_template,
+    request as req
+)
+import requests
+
+BACKEND_URL = "http://127.0.0.1:5000/tasks"
+
+app = Flaske(__name__)
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+
+@app.get("/tasks")
+def view_tasks():
+    response = requets.get(BACKEND_URL)
+    if response.status_code == 200:
+        task_list = response.json().get("tasks")
+        return render_template("list,html", tasks=task_list)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )
+
+@app.get("/tasks/<int:pk>/")
+def single_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+        task_data = response.json().get("task")
+        return render_template("detail.html", task=task_data)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )  
+
+@app.get("/tasks/new/")
+def new_form():
+    return render_template("new.html")
+
+@app.post("/tasks/new/")
+def create_task():
+    task_data = req.json
+    response = requests.post(BACKEND_URL, json=task_data)
+    if response.status_code == 204:
+        rturn render_template("success.html", message="Task created")
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )  
+
+@app.get("/tasks/<int:pk>/edit/")
+def edit_form(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requsts.get(url)
+    if response.status_code == 200:
+        task_data = response.json().get("task")
+        return render_template("edit.html", task=task_data)
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )   
+
+@app.post("/tasks/<int:pk>/edit/")
+def edit_task(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.put(url, json=req.json)
+    if response.status_code == 204:
+        return render_template("success.html", message="Task edited")
+    return (
+        render_template("error.html", error=response.status_code),
+        response.status_code
+    )    
+
+@app.get("/tasks/<int:pk>/delete/")
+def delete_form(pk):
+    url = "%s/%s" % (BACKEND_URL, pk)
+    response = requests.get(url)
+    if response.status_code == 200:
+        task_data = response.json().get("task")
+        return render_template("delete.html", task=task_data)
+    return (
+        render_template("error.html", error=response.status_code)
+        response.status_code
+    ) 
+
+    @app.post("/tasks/<int:pk>/delete/")
+    def delete_task(pk):
+        url = "%s/%s" % (BACKEND_URL, pk)
+        response = requests.delete(url)
+        if response.status_code == 204:
+            return render_template("success.html", message="Task deleted")
+        return (
+            render_template("error.html", error=response.status_code),
+            response.status_code
+        )       
